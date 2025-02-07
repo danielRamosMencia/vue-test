e36a65cc24ead8566ec6cb1f08be730682a53bb0
danielRamosMencia <danieldermramos@gmail.com>
2025-02-07 14:59:54 -0600

    feat: protect pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AboutView from "@/views/AboutView.vue";
 import LoginView from "@/views/LoginView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import UnauthorizedView from "@/views/UnauthorizedView.vue";
 import UsersView from "@/views/users/UsersView.vue";
 
 const router = createRouter({
@@ -16,21 +17,25 @@ const router = createRouter({
       path: "/users",
       name: "users",
       component: UsersView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/users/create",
       name: "users-create",
       component: () => import("@/views/users/CreateUserView.vue"),
+      meta: { requiresAuth: true },
     },
     {
       path: "/users/edit/:id",
       name: "users-edit",
-      component: () => import("@/views/users/CreateUserView.vue"), 
+      component: () => import("@/views/users/CreateUserView.vue"),
+      meta: { requiresAuth: true },
     },
     {
       path: "/users/delete/:id",
       name: "users-delete",
       component: () => import("@/views/users/DeleteUserView.vue"),
+      meta: { requiresAuth: true },
     },
     {
       path: "/about",
@@ -42,7 +47,22 @@ const router = createRouter({
       name: "not-found",
       component: NotFoundView,
     },
+    {
+      path: "/unauthorized",
+      name: "unauthorized",
+      component: UnauthorizedView,
+    },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+
+  if (to.meta.requiresAuth && !token) {
+    next({ name: "unauthorized" });
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ src/views/UnauthorizedView.vue
@@ -0,0 +1,7 @@
+<template>
+    <NotLogged />
+</template>
+
+<script setup lang="ts">
+import NotLogged from '@/components/NotLogged.vue';
+</script>
