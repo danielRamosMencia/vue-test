50f4c682d729e71ae6345c1fe6f3ffbf0a8898ec
danielRamosMencia <danieldermramos@gmail.com>
2025-02-07 00:42:54 -0600

    feat: adding zod to login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -5,22 +5,28 @@
         </div>
 
         <div class="flex justify-center items-center p-4 md:w-2/3">
-            <form class="border border-gray-300 bg-white shadow-md rounded px-8 pt-6 pb-8 w-full md:w-3/4 lg:w-1/2">
+            <form @submit.prevent="handleLogin"
+                class="border border-gray-300 bg-white shadow-md rounded px-8 pt-6 pb-8 w-full md:w-3/4 lg:w-1/2">
                 <span class="text-2xl font-bold text-center block mb-5">Inicia sesión</span>
+
                 <div class="mb-5">
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Usuario</label>
-                    <input type="text" id="email"
+                    <label for="usuario" class="block mb-2 text-sm font-medium text-gray-900">Usuario</label>
+                    <input v-model="form.usuario" type="text" id="usuario"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        placeholder="Ingresa tu usuario" required />
+                        placeholder="Ingresa tu usuario" />
+                    <p v-if="errors.usuario" class="text-red-600 text-sm">{{ errors.usuario }}</p>
                 </div>
+
                 <div class="mb-5">
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Contraseña</label>
-                    <input type="password" id="password"
+                    <input v-model="form.password" type="password" id="password"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        required placeholder="Ingresa tu contraseña" />
+                        placeholder="Ingresa tu contraseña" />
+                    <p v-if="errors.password" class="text-red-600 text-sm">{{ errors.password }}</p>
                 </div>
+
                 <div class="flex items-center justify-end">
-                    <button @click="login" type="submit"
+                    <button type="submit"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm w-full sm:w-auto px-5 py-2.5 text-center">Ingresar</button>
                 </div>
             </form>
@@ -30,10 +36,29 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { reactive } from 'vue';
+import { loginSchema } from '@/types/schemas/login-schema';
 
 const router = useRouter();
+const form = reactive({
+    usuario: '',
+    password: '',
+});
 
-const login = () => {
-    router.push("/users");
+const errors = reactive<{ [key: string]: string }>({});
+
+const handleLogin = () => {
+    const result = loginSchema.safeParse(form);
+
+    if (!result.success) {
+        errors.usuario = '';
+        errors.password = '';
+        result.error.issues.forEach((issue) => {
+            const field = issue.path[0] as string;
+            errors[field] = issue.message;
+        });
+    } else {
+        router.push("/users");
+    }
 }
 </script>
---
+++ src/types/schemas/login-schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  usuario: z.string().nonempty("El usuario es obligatorio"),
+  password: z
+    .string()
+    .nonempty("La contraseña es obligatoria")
+    .min(6, "La contraseña debe tener al menos 6 caracteres"),
+});
